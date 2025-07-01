3c86b524a73cc550e2badcd03cd9a5319154c4b4
anurag9601 <anuragmishrap13@gmail.com>
2025-07-01 23:06:31 +0530

    solved auth token error

--- app/(client)/create-blog/page.tsx
+++ app/(client)/create-blog/page.tsx
@@ -2,12 +2,38 @@
 
 import { useSession } from "next-auth/react";
 
-function CreateBlogPage(){
-    const data = useSession();
-    console.log(data);
-    return (
-        <div>Create Blog page</div>
-    )
-};
+function CreateBlogPage() {
+  const data = useSession();
+  return (
+    <div className="h-dvh w-dvw bg-black flex items-center justify-center">
+      <form className="flex flex-col items-center justify-center gap-3 max-w-[400px] w-full">
+        <label className="h-[50px] w-full border-2 border-white border-dashed rounded-lg cursor-pointer relative flex flex-col items-center justify-center">
+          <p className="text-white text-sm">Uplaod cover image</p>
+          <input type="file" className="invisible absolute" />
+        </label>
+        <div className="text-white flex flex-col gap-1 w-full">
+          <p>Title</p>
+          <input
+            type="text"
+            className="w-full h-[35px] border-1 border-white rounded-md text-white px-3"
+          />
+        </div>
+        <div className="text-white flex flex-col gap-1 w-full">
+          <p>Title</p>
+          <textarea
+            className="w-full border-1 border-white rounded-md text-white px-3 py-2"
+            rows={4}
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-white text-black px-[10px] py-[5px] rounded-md cursor-pointer w-full"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
 
 export default CreateBlogPage;
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,25 @@
 import { getToken } from "next-auth/jwt";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-    const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    const session = await getToken({ req, secret: process.env.AUTH_SECRET });
 
-    console.log("session", session);
+    const authURL = "/auth";
+    const protectedURLs = ["/create-blog"];
+
+    const pathName = req.nextUrl.pathname;
+
+    if (!session && protectedURLs.includes(pathName)) {
+        return NextResponse.redirect(new URL("/auth", req.url));
+    };
+
+    if (session && session.email && pathName === authURL) {
+        return NextResponse.redirect(new URL("/", req.url));
+    };
+
+    return NextResponse.next();
+};
+
+export const config = {
+    matcher: ["/dashboard/:path*", "/create-blog", "/auth"],
 };
